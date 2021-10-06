ff884830c19ab74f90c491ae9ae734acf3d2d9e2
AviYalow <yalow@g.jct.ac.il>
2021-10-06 12:59:17 +0300

    I am also here funcation

---
+++ Targil0/Program6094.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+
+  partial class Program
+{
+static partial void Wellcome6095()
+{
+
+Console.WriteLine("I am also here!");
+
+
+}
+}
+
+}
