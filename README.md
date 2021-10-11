428c16fd66d699ad218662f09bf5dace88e98a22
ohhanadavid <sustujbv167@gmail.com>
2021-10-11 13:57:14 +0300

    change massege

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+it's the best to work with you
 # dotNet5782_3417_6094
 mini project in c#
--- Targil0/Program6094.cs
+++ Targil0/Program6094.cs
@@ -13,7 +13,7 @@ namespace Targil0
         {
 
             Console.WriteLine("I am also here!");
-
+            
 
         }
     }
