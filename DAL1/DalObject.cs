2fafcb18a6cac3d12a5b6cb9869831a05189b663
ohhanadavid <sustujbv167@gmail.com>
2021-10-20 14:04:33 +0300

    add drone

--- DAL1/DalObject.cs
+++ DAL1/DalObject.cs
@@ -38,6 +38,26 @@ namespace DalObject
             DataSource.base_Stations[DataSource.Config.index_base_stations_empty].longitude = pointLine;
             DataSource.Config.index_base_stations_empty++;
         }
+        public static void Add_drone()
+        {
+            bool res;
+            int num;
+            double butrry;
+            Console.Write("Enter sireal number:");
+            res = int.TryParse(Console.ReadLine(), out num);
+            DataSource.drons[DataSource.Config.index_drowns_empty] = new Drone() { siralNumber = num  };
+            Console.Write("Enter name:");
+            DataSource.drons[DataSource.Config.index_drowns_empty].Model = Console.ReadLine();
+            Console.Write("Enter weight Category:0 for easy,1 for mediom,2 for heavy:");
+            res = int.TryParse(Console.ReadLine(), out num);
+            DataSource.drons[DataSource.Config.index_drowns_empty].weightCategory = (Weight_categories)num;
+            Console.Write("Enter amount of butrry:");
+            res =  double.TryParse(Console.ReadLine(), out butrry);
+            DataSource.drons[DataSource.Config.index_drowns_empty].butrryStatos = butrry;
+            Console.Write("Enter a statos:0 for free,1 for maintenance,2 for in charge:");
+            res = int.TryParse(Console.ReadLine(), out num);
+            DataSource.drons[DataSource.Config.index_drowns_empty].drownStatos = (Drone_statos)num;
+        }
 
     }
 }
