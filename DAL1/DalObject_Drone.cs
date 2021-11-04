dcb42361c9982087e248bef10a521b71f283a614
ohhanadavid <sustujbv167@gmail.com>
2021-11-04 22:23:50 +0200

    בדיקות תקינות ופיצול מחלקות

---
+++ DAL1/DalObject_Drone.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalObject;
+using IDAL.DO;
+
+namespace DAL
+{
+     partial class DalObject_Drone:DalObject.DalObject
+    {
+        public static bool sustainability_test(int number)
+        {
+            
+            foreach(Drone item in DataSource.drones)
+            {
+                if(item.serialNumber==number)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Adding a new drone
+        /// </summary>
+        /// <param name="siralNumber">Serial number of the drone</param>
+        /// <param name="model">The name of the modek </param>
+        /// <param name="category"> Easy / Medium / Heavy</param>
+        /// <param name="butrry">Battery status</param>
+        /// <param name="statos"> Free/ Maintenance/ Work</param>
+        public void Add_drone(int siralNumber, string model, int category, double butrry, int statos)
+        {
+            //  if (sustainability_test(droneNum))
+            // throw ("Error: The drone already exist in the system");
+            DataSource.drones.Add(new Drone()
+            {
+                serialNumber = siralNumber,
+                Model = model,
+                weightCategory = (Weight_categories)category
+            });
+
+        }
+
+        /// <summary>
+        /// Display drone data desired   
+        /// </summary>
+        /// <param name="droneNum">Desired drone number</param>
+        /// <returns> String of data</returns>
+        public Drone Drone_by_number(int droneNum)
+        {
+            //  if (!sustainability_test(droneNum))
+            // throw ("Error: The drone does not exist in the system");
+
+            foreach (Drone item in DataSource.drones)
+            {
+                if (item.serialNumber == droneNum)
+                {
+                    return item;
+
+                }
+            }
+            return DataSource.drones[0];
+        }
+
+        /// <summary>
+        /// Print all the drones
+        /// </summary>
+        /// <param name="array">A array list that will contain 
+        /// the values ​​of all the drones so we can print them</param>
+        public IEnumerable<Drone> Drone_list()
+        {
+            foreach (Drone drone in DataSource.drones)
+                yield return drone;
+        }
+
+        /// <summary>
+        /// delete a spsific drone
+        /// </summary>
+        /// <param name="sirial"></param>
+        public void DeleteDrone(int sirial)
+        {
+            //  if (!sustainability_test(droneNum))
+            // throw ("Error: The drone does not exist in the system");
+            for (int i = 0; i < DataSource.drones.Count(); i++)
+            {
+                if (DataSource.drones[i].serialNumber == sirial)
+                {
+                    DataSource.drones.Remove(DataSource.drones[i]);
+                    return;
+                }
+            }
+        }
+
+        public double[] Elctrtricity()
+        {
+            double[] elctricity = new double[5];
+            elctricity[0] = DataSource.Config.free;
+            elctricity[1] = DataSource.Config.easyWeight;
+            elctricity[2] = DataSource.Config.mediomWeight;
+            elctricity[3] = DataSource.Config.heavyWeight;
+            elctricity[4] = DataSource.Config.Charging_speed;
+            return elctricity;
+        }
+
+    }
+}
---
+++ DAL1/DalObject_Packege.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalObject;
+using IDAL.DO;
+
+namespace DAL
+{
+    partial class DalObject_Packege : DalObject.DalObject
+    {
+       public static bool sustainability_test(int number)
+        {
+
+            foreach (Package item in DataSource.packages)
+            {
+                if (item.serialNumber == number)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Adding a new package
+        /// </summary>
+        /// <param name="idsend">Sending customer ID</param>
+        /// <param name="idget">Receiving customer ID</param>
+        /// <param name="kg">Weight categories</param>
+        /// <param name="priorityByUser">Priority: Immediate/ quick/ Regular </param>
+        /// <returns>Returns the serial number of the created package</returns>
+        public int Add_package(int idsend, int idget, int kg, int priorityByUser)
+        {
+
+
+            DataSource.Config.package_num++;
+            Package package = new Package
+            {
+                serialNumber = DataSource.Config.package_num,
+                receiving_delivery = DateTime.Now,
+                sendClient = idsend,
+                getingClient = idget,
+                weightCatgory = (Weight_categories)kg,
+                priority = (Priority)priorityByUser
+            };
+
+            //  package.operator_skimmer_ID = found_drone_for_package(DataSource.packages[DataSource.Config.package_num].weightCatgory);
+
+            DataSource.packages.Add(package);
+            return DataSource.Config.package_num;
+        }
+
+        /// <summary>
+        /// connect package to drone
+        /// </summary>
+        /// <param name="packageNumber">serial number of the package that needs 
+        /// to connect to drone </param>
+        public void connect_package_to_drone(int packageNumber, int drone_sirial_number)
+        {
+            /* if (!sustainability_test(packageNumber))
+                 throw ("Error: The package does not exist in the system");*/
+            // if (!DalObject_Drone.sustainability_test(drone_sirial_number))
+                // throw ("Error: The package does not exist in the system");*/
+
+            int i = 0;
+            for (; i < DataSource.packages.Count(); i++)
+            {
+                if (DataSource.packages[i].serialNumber == packageNumber)
+                    break;
+            }
+            Package package = DataSource.packages[i];
+            package.operator_skimmer_ID = drone_sirial_number;
+
+            package.package_association = DateTime.Now;
+            DataSource.packages[i] = new Package
+            {
+                serialNumber = package.serialNumber,
+                sendClient = package.sendClient,
+                getingClient = package.getingClient,
+                operator_skimmer_ID = package.operator_skimmer_ID,
+                weightCatgory = package.weightCatgory,
+                package_arrived = package.package_arrived,
+                priority = package.priority,
+                receiving_delivery = package.receiving_delivery,
+                collect_package_for_shipment = package.collect_package_for_shipment,
+                package_association = package.package_association
+
+            };
+
+
+
+        }
+
+        /// <summary>
+        /// Updated package collected
+        /// </summary>
+        /// <param name="packageNumber">serial number of the package</param>
+        public void Package_collected(int packageNumber)
+        {
+            /* if (!sustainability_test(packageNumber))
+               throw ("Error: The package does not exist in the system");*/
+            int i = 0;
+            for (; i < DataSource.packages.Count(); i++)
+            {
+                if (DataSource.packages[i].serialNumber == packageNumber)
+                    break;
+            }
+            Package package = DataSource.packages[i];
+            package.collect_package_for_shipment = DateTime.Now;
+            DataSource.packages[i] = new Package
+            {
+                serialNumber = package.serialNumber,
+                sendClient = package.sendClient,
+                getingClient = package.getingClient,
+                operator_skimmer_ID = package.operator_skimmer_ID,
+                weightCatgory = package.weightCatgory,
+                package_arrived = package.package_arrived,
+                priority = package.priority,
+                receiving_delivery = package.receiving_delivery,
+                collect_package_for_shipment = package.collect_package_for_shipment,
+                package_association = package.package_association
+
+            };
+        }
+
+        /// <summary>
+        /// Update that package has arrived at destination
+        /// </summary>
+        /// <param name="packageNumber">serial number of the package</param>
+        public void Package_arrived(int packageNumber)
+        {
+            /* if (!sustainability_test(packageNumber))
+               throw ("Error: The package does not exist in the system");*/
+            int i = 0;
+            for (; i < DataSource.packages.Count(); i++)
+            {
+                if (DataSource.packages[i].serialNumber == packageNumber)
+                {
+                    break;
+                }
+
+            }
+            Package package = DataSource.packages[i];
+            package.package_arrived = DateTime.Now;
+            DataSource.packages[i] = new Package
+            {
+                serialNumber = package.serialNumber,
+                sendClient = package.sendClient,
+                getingClient = package.getingClient,
+                operator_skimmer_ID = package.operator_skimmer_ID,
+                weightCatgory = package.weightCatgory,
+                package_arrived = package.package_arrived,
+                priority = package.priority,
+                receiving_delivery = package.receiving_delivery,
+                collect_package_for_shipment = package.collect_package_for_shipment,
+                package_association = package.package_association
+
+            };
+            // drone_after_work(DataSource.packages[packageNumber - 1].operator_skimmer_ID);
+        }
+
+
+        /// <summary>
+        /// Display packege data desired
+        /// </summary>
+        /// <param name="packageNumbe">Serial number of a particular package</param>
+        /// <returns> string of data</returns>
+        public Package packege_by_number(int packageNumbe)
+        {
+            /* if (!sustainability_test(packageNumber))
+               throw ("Error: The package does not exist in the system");*/
+            foreach (Package package in DataSource.packages)
+                if (package.serialNumber == packageNumbe)
+                    return (package);
+            return DataSource.packages[0];
+
+        }
+
+        /// <summary>
+        /// Print all the packages
+        /// </summary>
+        /// <param name="array">A array list that will contain 
+        /// the values ​​of all the packages so we can print them</param>
+        public IEnumerable<Package> packege_list()
+        {
+            foreach (Package item in DataSource.packages)
+                yield return item;
+
+        }
+
+        /// <summary>
+        /// Displays a list of packages that
+        /// have not been assigned to a drone yet 
+        /// </summary>
+
+        public IEnumerable<Package> packege_list_with_no_drone()
+        {
+
+            foreach (Package item in DataSource.packages)
+            {
+                if (item.operator_skimmer_ID == 0)
+                    yield return item;
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// delete a spsific packege
+        /// </summary>
+        /// <param name="sirial"></param>
+        public void Deletepackege(int sirial)
+        {
+            /* if (!sustainability_test(packageNumber))
+               throw ("Error: The package does not exist in the system");*/
+            for (int i = 0; i < DataSource.packages.Count(); i++)
+            {
+                if (DataSource.packages[i].serialNumber == sirial)
+                {
+                    DataSource.packages.Remove(DataSource.packages[i]);
+                    return;
+                }
+            }
+        }
+
+    }
+}
