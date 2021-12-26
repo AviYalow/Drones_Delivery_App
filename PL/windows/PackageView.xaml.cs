559a060bbaa982e163e27b3414b35563ae8b6335
Avi Yalow <avrahamyalow@gmail.com>
2021-12-26 13:47:35 +0200

    new

--- PL/windows/PackageView.xaml.cs
+++ PL/windows/PackageView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PL.windows
+namespace PL
 {
     /// <summary>
     /// Interaction logic for PackageView.xaml
