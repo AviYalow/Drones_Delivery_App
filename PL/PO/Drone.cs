6d326fc3fb3816e2c0750ed549ba0c7e06f3e29e
ohhanadavid <sustujbv167@gmail.com>
2022-01-11 12:16:31 +0200

    start MVVM

---
+++ PL/PO/Drone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO
+{
+    /// <summary>
+    /// Drone
+    /// </summary>
+    public class Drone
+    {
+        public uint SerialNumber { get; init; }
+        public DroneModel Model { get; set; }
+
+        public WeightCategories WeightCategory { get; set; }
+        public PackageInTransfer PackageInTransfer { get; set; }
+
+        public double ButrryStatus { get; set; }
+        public DroneStatus DroneStatus { get; set; }
+        public Location Location { get; set; }
+        public override string ToString()
+        {
+            String print = "";
+            print += $"Serial Number: {SerialNumber},\n";
+            print += $"model: {Model},\n";
+            print+= $"Weight Category: {WeightCategory},\n";
+            print += $"Package in transfer:\n";
+            print +=(PackageInTransfer != null)?  $" {PackageInTransfer,3}":'0';
+            print += $"Butrry status: {ButrryStatus},\n";
+            print += $"Drone status: {DroneStatus},\n";
+            print += $"Location:\n{Location}\n";
+           
+            return print;
+        }
+
+    }
+}
